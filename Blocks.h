91cd4633c5e819f636ac354495d73760e043612b
MaksimKhoruzhenko <114007431+8elar8oy@users.noreply.github.com>
2023-01-21 19:47:33 +0300

    Add files via upload

--- Blocks.h
+++ Blocks.h
@@ -7,13 +7,30 @@ struct Block {
 	Vector2f position;
 	
 };
+struct BlockRow {
+	Block arr[10];
+	int size;
 
+};
+
+void BlockInit(BlockRow& blockRow,int size,Color& color, Vector2f& position,float stepX) {
+	blockRow.size = size;
+	for (int i = 0; i < blockRow.size; i++)
+	{
+		sf::Color color{ (sf::Uint8)(25 * i),100,100 };
+		sf::Vector2f blockPos{ position.x + i * stepX,position.y };
+		initBlock(blockRow.arr[i],color,blockPos);
+	}
+	
+}
 void initBlock(Block& block,Color& color, Vector2f& position) {
+	
 	block.color = color;
 	block.position = position;
 	block.shape.setSize(BLOCK_SIZE);
 	block.shape.setFillColor(block.color);
 	block.shape.setPosition(block.position);
+
 }
 
 void updateBlock(Block& block) {
